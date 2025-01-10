a113cd2c85a63e7ad66b8291431d7c74b3300267
Teodora Vermesan <vermesanteodora@gmail.com>
2025-01-10 14:15:50 +0200

    test

--- cypress.config.js
+++ cypress.config.js
@@ -2,22 +2,22 @@ const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
-  reporter: "cypress-mochawesome-reporter",
-  video: true,
-  retries: 1,
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: "Test Execution Report",
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-  },
+  // reporter: "cypress-mochawesome-reporter",
+  // video: true,
+  // retries: 1,
+  // reporterOptions: {
+  //   charts: true,
+  //   reportPageTitle: "Test Execution Report",
+  //   embeddedScreenshots: true,
+  //   inlineAssets: true,
+  //   saveAllAttempts: false,
+  // },
   e2e: {
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
       on("file:preprocessor", cucumber());
     },
-    testIsolation: true,
+    testIsolation: false,
     specPattern: "cypress/e2e",
     supportFile: false,
   },

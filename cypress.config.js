86d540a9db0a48b7a5fc8a0183def2670547bd19
Teodora Vermesan <vermesanteodora@gmail.com>
2025-01-10 12:54:01 +0200

    test

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,22 @@ const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
+  video: true,
+  retries: 1,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Test Execution Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      //require("cypress-mochawesome-reporter/plugin")(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
       on("file:preprocessor", cucumber());
     },
     testIsolation: false,
     specPattern: "cypress/e2e",
-    // supportFile: false,
+    supportFile: false,
   },
 });
---
+++ cypress/cucumberReport.js
@@ -0,0 +1,16 @@
+const report = require("multiple-cucumber-html-reporter");
+
+report.generate({
+  jsonDir: "./reports/cucumber-json",
+  reportPath: "./reports/cucumber-htmlreport.html",
+  customData: {
+    title: "Run info",
+    data: [
+      { label: "Project", value: "Custom project" },
+      { label: "Release", value: "1.2.3" },
+      { label: "Cycle", value: "B11221.34321" },
+      { label: "Execution Start Time", value: "Nov 19th 2017, 02:31 PM EST" },
+      { label: "Execution End Time", value: "Nov 19th 2017, 02:56 PM EST" },
+    ],
+  },
+});

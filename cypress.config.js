b5952d3fa90868cf6ee243ca85963ccec9a6f94f
Teodora Vermesan <vermesanteodora@gmail.com>
2025-01-10 10:11:06 +0200

    change

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
       on("file:preprocessor", cucumber());
     },
     //  testIsolation: false,
-    specPattern: "cypress/e2e/*.feature",
+    specPattern: "cypress/e2e/",
     // supportFile: false,
   },
 });
